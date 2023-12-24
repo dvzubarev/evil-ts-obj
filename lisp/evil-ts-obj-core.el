3f676e62fb07706c1cfcb03c6adca6a1cd11171d
Denis Zubarev <dvzubarev@yandex.ru>
2023-12-25 00:49:49 +0300

    core: prefer largest node when point is at start
    of multiple things

--- lisp/evil-ts-obj-core.el
+++ lisp/evil-ts-obj-core.el
@@ -155,22 +155,26 @@ node, if it exists. Return nil if no node can be found."
 `THING' should be a thing defined in `treesit-thing-settings'. If
 there is no enclosing thing, jump to the next one unless
 `DONT-STEP-FORWARD'. This function also handles the case when
-multiple things start at the same position. e.g. in yaml - item1
-Hyphen symbol is the beginning of the list and a list item. When
+multiple things start at the same position. For example, yaml:
+
+- item1
+
+ Hyphen symbol is the beginning of the list and a list item. When
 there are multiple overlapping things, the result depends on
 `POS'. If `POS' is inside of any thing, then the smallest
-enclosing thing is returned. If the `POS' is before any thing (on
-the same line), function returns the largest node that starts
-after `POS'. If the `POS' is after any thing, function returns
-the largest node that ends before `POS'. It returns nil if no
-thing can be found (e.g. empty line)."
+enclosing thing is returned. If the `POS' is at the start of
+multiple things or before any thing (on the same line), function
+returns the largest node that starts at or after `POS'. If the `POS' is
+after any thing, function returns the largest node that ends
+before `POS'. It returns nil if no thing can be found (e.g. empty
+line)."
 
   (let* ((cursor (evil-ts-obj--node-at-or-around pos))
          (iter-pred (lambda (node)
                       (treesit-node-match-p node thing t)))
          (enclosing-node (treesit-parent-until cursor iter-pred t))
          (before-cursor (and enclosing-node
-                             (< pos (treesit-node-start enclosing-node))))
+                             (<= pos (treesit-node-start enclosing-node))))
          (after-cursor (and enclosing-node
                             (<= (treesit-node-end enclosing-node) pos))))
 
